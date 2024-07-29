22ac2dbd23698cda9d0e497638b293801b37c589
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-07-29 16:19:50 +0200

    Init commit

---
+++ src/main/java/com/example/nc1_test_task/Nc1TestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nc1_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nc1TestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nc1TestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nc1_test_task/Nc1TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nc1_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nc1TestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
