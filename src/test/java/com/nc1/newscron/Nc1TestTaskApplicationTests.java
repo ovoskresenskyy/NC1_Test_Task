1a8df047aa6f6e11939fad366960c5bed526bf87
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-07-30 10:35:25 +0200

    Fixed checkstyle

--- src/test/java/com/nc1/newscron/Nc1TestTaskApplicationTests.java
+++ src/test/java/com/nc1/newscron/Nc1TestTaskApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.nc1_test_task;
+package com.nc1.newscron;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Nc1TestTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
